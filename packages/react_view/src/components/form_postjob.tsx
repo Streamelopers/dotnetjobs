32c745658da589853213c9faa501f75c76ce1fd4
Manases Lovera <manaseslovera07@gmail.com>
2024-07-29 02:37:17 -0400

    feat: styles to the Job Submission Form DONE

--- packages/react_view/src/components/form_postjob.tsx
+++ packages/react_view/src/components/form_postjob.tsx
@@ -1,12 +1,12 @@
-
+import '../styles/form_postjob.css'
 
 function Form_PostJob() {
     return (
-      <>
-        <form>
-            <h4>
+      <div className='form_container'>
+        <h4 className='form_postjob_subtitle'>
             Job Submission Form
-            </h4>
+        </h4>
+        <form className='form_postjob'>
             <div className="form_row">
             <div className="form_column">
                 <label htmlFor="">Job Title</label>
@@ -15,9 +15,10 @@ function Form_PostJob() {
             <div className="form_column">
                 <label htmlFor="">Job Type</label>
                 <select name="" id="">
-                <option value="">Fulltime</option>
-                <option value="">Part-time</option>
-                <option value="">Internship</option>
+                <option value="">Select</option>
+                <option value="Fulltime">Fulltime</option>
+                <option value="Part-time">Part-time</option>
+                <option value="Internship">Internship</option>
                 </select>
             </div>
             <div className="form_column">
@@ -33,8 +34,8 @@ function Form_PostJob() {
             <div className="form_column">
                 <label htmlFor="">Salary</label>
                 <div className="salary_input">
-                <input type="number" name="" id="" placeholder='Min'/>
-                <input type="number" name="" id="" placeholder='Max'/>
+                    <input type="number" name="" id="" placeholder='Min'/>
+                    <input type="number" name="" id="" placeholder='Max'/>
                 </div>
             </div>
             <div className="form_column">
@@ -42,16 +43,16 @@ function Form_PostJob() {
                 <input type="text" name="" id="" />
             </div>
             </div>
-            <div className="form_row">
-            <label htmlFor="">Job Description</label>
-            <textarea name="" id="" rows={4} cols={40}></textarea>
+            <div className="form_column">
+                <label htmlFor="">Job Description</label>
+                <textarea name="" id="" rows={7} cols={40}></textarea>
             </div>
             <div className="form_row">
             <input type="file" name="" id="" placeholder='Upload Files' />
             </div>
         </form>
-        <button>+ Post a Job</button>
-      </>
+        <button className='postjob_btn'>+ Post a Job</button>
+      </div>
     )
 }
 
--- packages/react_view/src/views/PostJob.tsx
+++ packages/react_view/src/views/PostJob.tsx
@@ -1,16 +1,14 @@
 //import './PostJob.css'
 import Nav from '../components/nav.tsx'
 import Form_PostJob from '../components/form_postjob.tsx'
+import '../styles/postjob.css'
 
 function PostJob() {
 
   return (
     <>
       <Nav />
-      <main>
-        <Form_PostJob />
-      </main>
-      
+      <Form_PostJob />
     </>
   )
 }
