38d6460dc9d64e6684250f3a6744446c07979df6
Enmanuel Toribio <enmanuel@WindowsPC>
2024-08-03 13:08:08 -0400

    Adding scripts for templates

---
+++ AppServices/Data/Repositories/BaseRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppServices.Data.Repositories
+{
+    public class BaseRepository
+    {
+    }
+}
